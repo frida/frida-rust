78e9cdd2f73f76bae78392918ce712c43042cf92
s1341 <github@shmarya.net>
2022-08-01 09:03:58 +0300

    Move to try_into. Doh

--- frida-gum/src/instruction_writer.rs
+++ frida-gum/src/instruction_writer.rs
@@ -604,7 +604,7 @@ impl X86InstructionWriter {
         unsafe {
             gum_sys::gum_x86_writer_put_jcc_short_label(
                 self.writer,
-                condition as _ as i32,
+                condition.try_into().unwrap(),
                 label_id as *const c_void,
                 hint,
             )
@@ -620,7 +620,7 @@ impl X86InstructionWriter {
         unsafe {
             gum_sys::gum_x86_writer_put_jcc_near_label(
                 self.writer,
-                condition as _ as i32,
+                condition.try_into().unwrap(),
                 label_id as *const c_void,
                 hint,
             )
