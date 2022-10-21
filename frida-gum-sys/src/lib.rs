26d286e53e6013b313b66beff6b6572890aa0b93
t4lz <t4lz.git@gmail.com>
2022-10-21 18:03:49 +0200

    Don't use non existing name on linux-arm64.

--- frida-gum-sys/src/lib.rs
+++ frida-gum-sys/src/lib.rs
@@ -20,6 +20,7 @@ pub use bindings::*;
     target_os = "macos",
     target_os = "ios",
     target_os = "windows",
-    target_os = "android"
+    target_os = "android",
+    all(target_os = "linux", target_arch = "aarch64")
 )))]
 pub use _frida_g_object_unref as g_object_unref;
