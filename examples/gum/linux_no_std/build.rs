f06638e7ae228edc04a4554498827c435dfb70df
Your Name <you@example.com>
2023-03-11 16:06:08 +0000

    Fix comments and test name

--- examples/gum/linux_no_std/build.rs
+++ examples/gum/linux_no_std/build.rs
@@ -1,12 +1,8 @@
 fn main() {
-    /*
-     * By default a no_std target will not link against libc. We want libc for
-     * our target since FRIDA requires it and also we need to use the function
-     * `abort`
-     */
+    // By default a no_std target will not link against libc. We want libc for
+    // our target since FRIDA requires it and also we need to use the function
+    // `abort`
     println!("cargo:rustc-link-arg=-lc");
-    /*
-     * Also link against the library containing the exception handlers for GCC.
-     */
+    // Also link against the library containing the exception handlers for GCC.
     println!("cargo:rustc-link-arg=-lgcc_eh");
 }
