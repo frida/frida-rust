fc21fa5d6e037f2184696c09b7dfb14f276cd3b3
Sharad <sharad@mineo333.dev>
2024-12-13 19:06:10 -0500

    Link on all apple OSs

--- frida-gum-sys/build.rs
+++ frida-gum-sys/build.rs
@@ -243,6 +243,6 @@ fn main() {
     #[cfg(all(feature = "js", target_os = "macos"))]
     println!("cargo:rustc-link-lib=dylib=c++");
 
-    #[cfg(target_os = "macos")]
+    #[cfg(target_vendor = "apple")]
     println!("cargo:rustc-link-lib=dylib=resolv");
 }
