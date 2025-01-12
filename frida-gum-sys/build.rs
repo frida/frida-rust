fa0ffbf93830f151a7b04f8e2ea7abfceb9dd066
s1341 <github@shmarya.net>
2025-01-12 08:19:14 +0200

    macos: remove dependency on c++

--- frida-gum-sys/build.rs
+++ frida-gum-sys/build.rs
@@ -241,8 +241,5 @@ fn main() {
     println!("cargo:rustc-link-lib=dylib=stdc++");
 
     #[cfg(all(feature = "js", target_os = "macos"))]
-    println!("cargo:rustc-link-lib=dylib=c++");
-
-    #[cfg(all(feature = "js", target_os = "macos"))]
     println!("cargo:rustc-link-lib=resolv");
 }
