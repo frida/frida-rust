4ab0878582488b570e92b0735ddbf3c9013a9a76
cc <codecolorist@gmail.com>
2025-02-16 01:56:33 +0100

    Device: add example for application api

---
+++ examples/core/get_apps/src/main.rs
@@ -0,0 +1,26 @@
+use frida::{Scope, DeviceType};
+
+fn main() {
+    let frida = unsafe { frida::Frida::obtain() };
+    let device_manager = frida::DeviceManager::obtain(&frida);
+    let device = device_manager.get_device_by_type(DeviceType::USB).unwrap();
+    assert_eq!(device.get_type(), DeviceType::USB);
+    let apps = device.enumerate_applications(None, None);
+
+    if !apps.is_empty() {
+        // query detail of the first app
+        let identifier = apps[0].get_identifier();
+        let single = device.enumerate_applications(Some(&[identifier]), Some(Scope::Full));
+        let first = single.first().unwrap();
+        let params = first.get_parameters().unwrap();
+
+        println!("parameters of {}: ", first.get_identifier());
+        for (k, v) in params {
+            println!("{}: {:?}", k, v);
+        }
+    }
+
+    for a in apps {
+        println!("{} {} {:?}", a.get_name(), a.get_identifier(), a.get_pid());
+    }
+}
