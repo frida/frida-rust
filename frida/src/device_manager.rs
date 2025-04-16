14881d458ee6b7c6e296caa9f403179a6e10e2ae
Rubens Brandão <git@rubens.io>
2025-04-16 01:05:55 -0300

    fix `frida::DeviceManager::get_device_by_id` documentation (#203)

--- frida/src/device_manager.rs
+++ frida/src/device_manager.rs
@@ -115,14 +115,14 @@ impl<'a> DeviceManager<'a> {
     /// Returns the device with the specified id.
     ///
     /// # Example
-    ///
+    /// ```no_run
     /// let frida = unsafe { frida::Frida::obtain() };
     /// let device_manager = frida::DeviceManager::obtain(&frida);
     ///
     /// let id = "<some id>";
     /// let device = device_manager.get_device_by_id(id).unwrap();
     /// assert_eq!(device.get_id(), id);
-    ///
+    /// ```
     pub fn get_device_by_id(&'a self, device_id: &str) -> Result<Device<'a>> {
         let mut error: *mut frida_sys::GError = std::ptr::null_mut();
         let cstring = CString::new(device_id).unwrap();
