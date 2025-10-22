82a4ab5752ac4f63d38f5d2e5f74bd5286eea98d
b1ueb1ues <bblues@live.cn>
2025-10-22 15:51:38 +0800

    update examples/gum to frida17 Module api

--- examples/gum/open/src/lib.rs
+++ examples/gum/open/src/lib.rs
@@ -3,7 +3,7 @@
 use frida_gum as gum;
 use gum::{
     interceptor::{Interceptor, InvocationContext, InvocationListener},
-    Gum, Module,
+    Gum, Module, Process,
 };
 use std::os::raw::{c_int, c_void};
 use std::sync::OnceLock;
@@ -29,16 +29,18 @@ extern "C" fn example_agent_main(_user_data: *const c_void, resident: *mut c_int
 
     let mut interceptor = Interceptor::obtain(gum);
     let mut listener = OpenListener {};
-
-    let module = Module::obtain(gum);
-    let modules = module.enumerate_modules();
+    
+    let process = Process::obtain(gum);
+    let modules = process.enumerate_modules();
     for module in modules {
         println!(
             "{}@{:#x}/{:#x}",
-            module.name, module.base_address, module.size
+            module.name(),
+            module.range().base_address(),
+            module.range().size()
         );
     }
 
-    let open = module.find_export_by_name(None, "open").unwrap();
+    let open = Module::find_global_export_by_name("open").unwrap();
     interceptor.attach(open, &mut listener).unwrap();
 }
