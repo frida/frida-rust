e98b64fc36a172095e345ba06c7c19f2676d7a51
ykuszer <yossefkuszer@gmail.com>
2025-11-11 06:59:12 +0200

    Adding support for gum_backtracer_generate_with_limit in Backtrace (#223)

--- frida-gum/src/backtracer.rs
+++ frida-gum/src/backtracer.rs
@@ -33,18 +33,20 @@ extern "C" {
 pub struct Backtracer;
 
 impl Backtracer {
-    /// Generate a backtrace
-    fn generate(
+    /// Generate a backtrace stopping after 'limit' entries
+    fn generate_with_limit(
         backtracer: *mut gum_sys::GumBacktracer,
         context: *const gum_sys::GumCpuContext,
+        limit: gum_sys::guint,
     ) -> Vec<usize> {
         let mut return_address_array = MaybeUninit::<gum_sys::_GumReturnAddressArray>::uninit();
 
         unsafe {
-            gum_sys::gum_backtracer_generate(
+            gum_sys::gum_backtracer_generate_with_limit(
                 backtracer,
                 context,
                 return_address_array.as_mut_ptr(),
+                limit,
             );
             let return_address_array = return_address_array.assume_init();
             let mut result = vec![];
@@ -57,35 +59,69 @@ impl Backtracer {
 
     /// Generate an accurate backtrace as a list of return addresses from the current context
     pub fn accurate() -> Vec<usize> {
-        Self::generate(
+        Self::accurate_with_limit(gum_sys::GUM_MAX_BACKTRACE_DEPTH)
+    }
+
+    /// Generate an accurate backtrace as a list of return addresses from the current context
+    /// stopping after 'limit' entries
+    pub fn accurate_with_limit(limit: u32) -> Vec<usize> {
+        Self::generate_with_limit(
             unsafe { gum_sys::gum_backtracer_make_accurate() },
             core::ptr::null(),
+            limit,
         )
     }
 
     /// Generate a fuzzy backtrace as a list of return addresses from the current context
     pub fn fuzzy() -> Vec<usize> {
-        Self::generate(
+        Self::fuzzy_with_limit(gum_sys::GUM_MAX_BACKTRACE_DEPTH)
+    }
+
+    /// Generate a fuzzy backtrace as a list of return addresses from the current context stopping
+    /// after 'limit' entries.
+    pub fn fuzzy_with_limit(limit: u32) -> Vec<usize> {
+        Self::generate_with_limit(
             unsafe { gum_sys::gum_backtracer_make_fuzzy() },
             core::ptr::null(),
+            limit,
         )
     }
 
     /// Generate an accurate backtrace as a list of return addresses for the supplied cpu
     /// context.
     pub fn accurate_with_context(context: &gum_sys::GumCpuContext) -> Vec<usize> {
-        Self::generate(
+        Self::accurate_with_context_and_limit(context, gum_sys::GUM_MAX_BACKTRACE_DEPTH)
+    }
+
+    /// Generate an accurate backtrace as a list of return addresses for the supplied cpu
+    /// context stopping after 'limit' entries.
+    pub fn accurate_with_context_and_limit(
+        context: &gum_sys::GumCpuContext,
+        limit: u32,
+    ) -> Vec<usize> {
+        Self::generate_with_limit(
             unsafe { gum_sys::gum_backtracer_make_accurate() },
             context as *const gum_sys::GumCpuContext,
+            limit,
         )
     }
 
     /// Generate a fuzzy backtrace as a list of return addresses for the supplied cpu
     /// context.
     pub fn fuzzy_with_context(context: &gum_sys::GumCpuContext) -> Vec<usize> {
-        Self::generate(
+        Self::fuzzy_with_context_and_limit(&context, gum_sys::GUM_MAX_BACKTRACE_DEPTH)
+    }
+
+    /// Generate a fuzzy backtrace as a list of return addresses for the supplied cpu
+    /// context stopping after 'limit' entries.
+    pub fn fuzzy_with_context_and_limit(
+        context: &gum_sys::GumCpuContext,
+        limit: u32,
+    ) -> Vec<usize> {
+        Self::generate_with_limit(
             unsafe { gum_sys::gum_backtracer_make_fuzzy() },
             context as *const gum_sys::GumCpuContext,
+            limit,
         )
     }
 
@@ -93,6 +129,16 @@ impl Backtracer {
     /// context.
     #[cfg(any(target_os = "linux", target_os = "freebsd"))]
     pub fn accurate_with_signal_context(context: &libc::ucontext_t) -> Vec<usize> {
+        Self::accurate_with_signal_context_and_limit(context, gum_sys::GUM_MAX_BACKTRACE_DEPTH)
+    }
+
+    /// Generate an accurate backtrace as a list of return addresses for the supplied signal
+    /// context stopping after 'limit' entries.
+    #[cfg(any(target_os = "linux", target_os = "freebsd"))]
+    pub fn accurate_with_signal_context_and_limit(
+        context: &libc::ucontext_t,
+        limit: u32,
+    ) -> Vec<usize> {
         let mut cpu_context = MaybeUninit::<gum_sys::GumCpuContext>::uninit();
 
         unsafe {
@@ -103,14 +149,25 @@ impl Backtracer {
                 context as *const libc::ucontext_t,
                 cpu_context.as_mut_ptr(),
             );
-            Self::accurate_with_context(&cpu_context.assume_init())
+            Self::accurate_with_context_and_limit(&cpu_context.assume_init(), limit)
         }
     }
 
     /// Generate a fuzzy backtrace as a list of return addresses for the supplied signal
     /// context.
     #[cfg(any(target_os = "linux", target_os = "freebsd"))]
+
     pub fn fuzzy_with_signal_context(context: &libc::ucontext_t) -> Vec<usize> {
+        Self::fuzzy_with_signal_context_and_limit(context, gum_sys::GUM_MAX_BACKTRACE_DEPTH)
+    }
+
+    /// Generate a fuzzy backtrace as a list of return addresses for the supplied signal
+    /// context stopping after 'limit' entries
+    #[cfg(any(target_os = "linux", target_os = "freebsd"))]
+    pub fn fuzzy_with_signal_context_and_limit(
+        context: &libc::ucontext_t,
+        limit: u32,
+    ) -> Vec<usize> {
         let mut cpu_context = MaybeUninit::<gum_sys::GumCpuContext>::uninit();
 
         unsafe {
@@ -121,7 +178,7 @@ impl Backtracer {
                 context as *const libc::ucontext_t,
                 cpu_context.as_mut_ptr(),
             );
-            Self::fuzzy_with_context(&cpu_context.assume_init())
+            Self::fuzzy_with_context_and_limit(&cpu_context.assume_init(), limit)
         }
     }
 }
